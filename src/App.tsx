01825109ba69c0cb140326062889e526a2b24436
GersonEC <gersonenriquez94@gmail.com>
2022-03-07 17:16:15 +0100

    Implement todo app with custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useCallback, useReducer, useRef} from "react";
+import { useTodos } from './useTodos';
+
+const Heading = (props: { title: string }) => {
+  return (
+    <h2>
+      <p>{props.title}</p>
+    </h2>
+  );
+};
 
 function App() {
+  const onListClick = useCallback((item: string) => {
+    alert(item);
+  }, []);
+  const newTodoRef = useRef<HTMLInputElement>(null);
+  const { todos, addTodo, removeTodo } = useTodos([
+    {id: 0, text: 'Hey There', done: false}
+  ])
+
+  const onAddTodo = useCallback(() => {
+    if(newTodoRef.current) {
+      addTodo(newTodoRef.current.value);
+      newTodoRef.current.value = "";
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: 'gray', height: '100vh'}}>
+      <Heading title="Introduction" />
+      <div>
+        <input type="text" ref={newTodoRef} />
+        <button onClick={onAddTodo}>Add Todo</button>
+      </div>
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          {todo.text}
+          <button
+            onClick={() => removeTodo(todo.id)
+            }
+          >
+            Remove
+          </button>
+        </div>
+      ))}
     </div>
   );
 }

072f60fbe6e0fe5ef94226ff70506cad0be343da
GersonEC <gersonenriquez94@gmail.com>
2022-03-16 09:18:17 +0100

    App Indentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, {useCallback, useReducer, useEffect, useRef, useState} from "react";
-import { useTodos, useAddTodo, useRemoveTodo, TodosProvider } from './useTodos';
+import React, { useCallback, useRef } from "react";
+import { useTodos, useAddTodo, useRemoveTodo, TodosProvider } from "./useTodos";
 
 const Heading = (props: { title: string }) => {
   return (
@@ -9,18 +9,24 @@ const Heading = (props: { title: string }) => {
   );
 };
 
-function UL<T>({ items, render, itemClick }: {
-  items: T[],
+function UL<T>({
+  items,
+  render,
+  itemClick,
+}: {
+  items: T[];
   itemClick: (item: T) => void;
   render: (item: T) => React.ReactNode;
 }) {
-  return(
+  return (
     <ul>
       {items.map((item, index) => (
-        <li key={index} onClick={() => itemClick(item)}>{render(item)}</li>
+        <li key={index} onClick={() => itemClick(item)}>
+          {render(item)}
+        </li>
       ))}
     </ul>
-  )
+  );
 }
 
 function App() {
@@ -34,14 +40,14 @@ function App() {
   const removeTodo = useRemoveTodo();
 
   const onAddTodo = useCallback(() => {
-    if(newTodoRef.current) {
+    if (newTodoRef.current) {
       addTodo(newTodoRef.current.value);
       newTodoRef.current.value = "";
     }
   }, []);
 
   return (
-    <div style={{ backgroundColor: 'gray', height: '100vh'}}>
+    <div style={{ backgroundColor: "gray", height: "100vh" }}>
       <Heading title="Introduction" />
       <div>
         <input type="text" ref={newTodoRef} />
@@ -50,19 +56,13 @@ function App() {
       <UL
         items={todos}
         itemClick={(item) => alert(item.id)}
-        render={ (todo) => (
+        render={(todo) => (
           <>
             {todo.text}
-            <button
-              onClick={() => removeTodo(todo.id)
-              }
-            >
-              Remove
-            </button>
+            <button onClick={() => removeTodo(todo.id)}>Remove</button>
           </>
         )}
       />
-    
     </div>
   );
 }
@@ -71,29 +71,24 @@ const JustShowTodos = () => {
   const todos = useTodos();
   return (
     <UL
-        items={todos}
-        itemClick={(item) => alert(item.id)}
-        render={ (todo) => (
-          <>
-            {todo.text}
-            
-          </>
-        )}
-      />
-  )
-}
+      items={todos}
+      itemClick={(item) => alert(item.id)}
+      render={(todo) => <>{todo.text}</>}
+    />
+  );
+};
 
 const AppWrapper = () => (
-  <TodosProvider initialTodos={[
-    {id: 0, text: 'Hey There', done: false}
-  ]}>
-    <div style={{
-      display: "grid",
-      gridTemplateColumns: "50% 50%",
-    }}>
+  <TodosProvider initialTodos={[{ id: 0, text: "Hey There", done: false }]}>
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "50% 50%",
+      }}
+    >
       <App></App>
       <JustShowTodos />
     </div>
   </TodosProvider>
-)
+);
 export default AppWrapper;
